3e30885e698e26314c989112d3dc92042ecf874a
ConsolAktif <sigortataciri@gmail.com>
2025-01-03 17:50:02 +0300

    jumbotron is added.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ImageSlider from "../../components/ImageSlider/ImageSlider";
+import Jumbotron from "../../components/Jumbotron/Jumbotron";
 
 const Home = () => {
+
   return (
     <div className="bg-white dark:bg-gray-900">
       <ImageSlider />
+      <Jumbotron />
     </div>
   );
 };
