264ed0ab825349c24c623ea663078111db69de60
ConsolAktif <sigortataciri@gmail.com>
2025-01-04 17:56:53 +0300

    toast container was added.

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import "./index.css";
 import App from "./App.jsx";
 import Navbar from "./pages/Navbar/Navbar.jsx";
 import Footer from "./pages/Footer/Footer.jsx";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Navbar />
-    <App />
-    <Footer />
+    <div className="bg-gray-900 dark:bg-gray-900 h-screen">
+      <ToastContainer />
+      <Navbar />
+      <App />
+      <Footer />
+    </div>
   </StrictMode>
 );
