4718a1ee22ab6c036d607907d5b42472de6c52b4
ConsolAktif <sigortataciri@gmail.com>
2025-01-24 10:04:39 +0300

    new update.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// vite.config.js
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: "esnext", // Modern tarayıcılar için optimize et
+    outDir: "dist", // Çıktı dizini
+    rollupOptions: {
+      output: {
+        format: "es", // Modül formatı olarak ES kullan
+      },
+    },
+  },
   server: {
     host: "0.0.0.0",
-    allowedHosts: ["consolaktif.onrender.com"],
     port: 3000,
   },
 });
