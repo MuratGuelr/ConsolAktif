8738a8ed8d3852e7e9d6e1efcf909f88826cf514
ConsolAktif <sigortataciri@gmail.com>
2025-04-25 11:01:02 +0300

    Hero section added.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,13 @@
 export default function Hero() {
   return (
-    <div className="hero bg-base-200 min-h-screen">
+    <div className="hero bg-base-200 pt-10">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Hello there</h1>
+          <h1 className="text-5xl font-bold">Merhaba 👋</h1>
           <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            Websitem üzerinden uygulamalarıma ulaşabilir, yeni videolarımdan
+            haberdar olabilir veya yeni haberlere ulaşabilirsin.{" "}
+            <span className="text-xs text-base-primary">(Şüpheli : 🤔)</span>
           </p>
           <button className="btn btn-primary">Get Started</button>
         </div>
