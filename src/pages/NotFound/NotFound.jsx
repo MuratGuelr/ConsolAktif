be06395f196f1637f70904599b30249e3d8ef1cb
ConsolAktif <sigortataciri@gmail.com>
2025-04-25 11:00:23 +0300

    NotFound page added.

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,24 +1,20 @@
 export default function NotFound() {
   return (
     <>
-      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8 bg-base-300">
         <div className="text-center">
           <p className="text-base font-semibold text-indigo-600">404</p>
-          <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-            Page not found
+          <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance sm:text-7xl text-neutral-content">
+            Aradığınız sayfayı bulamadık 😔
           </h1>
-          <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-            Sorry, we couldn’t find the page you’re looking for.
+          <p className="mt-6 text-lg font-medium text-pretty sm:text-xl/8 text-gray-400">
+            Ancak sizi aşağıdaki "
+            <button className="btn btn-soft btn-accent btn-xs">Button</button>"
+            ile geri gönderebiliriz 😁
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
-            <a
-              href="#"
-              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Go back home
-            </a>
-            <a href="#" className="text-sm font-semibold text-gray-900">
-              Contact support <span aria-hidden="true">&rarr;</span>
+            <a href="/" className="btn btn-soft btn-accent">
+              Anasayfaya Dön
             </a>
           </div>
         </div>
