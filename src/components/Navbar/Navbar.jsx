884f8dcd993adf8a78e7531f3c3cebc3b45b099f
ConsolAktif <sigortataciri@gmail.com>
2025-04-25 11:01:36 +0300

    Navbar component added.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,49 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import Avatar from "../Avatar/Avatar";
 
 const Navbar = () => {
+  const [user, setUser] = useState(false);
+
   return (
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar bg-base-200 shadow-sm">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a href="/" className="btn btn-ghost text-xl">
+          ConsolAktif
+        </a>
       </div>
-      <div className="flex gap-2">
-        <input
-          type="text"
-          placeholder="Search"
-          className="input input-bordered w-24 md:w-auto"
-        />
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-              />
-            </div>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
+
+      {!user && (
+        <div className="flex gap-2">
+          <a href="login">
+            <button className="btn btn-soft btn-primary">Giriş Yap</button>
+          </a>
         </div>
-      </div>
+      )}
+
+      {user && (
+        <div className="flex gap-2">
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <Avatar />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a className="justify-between">
+                  Profil
+                  <span className="badge">Yeni</span>
+                </a>
+              </li>
+              <li>
+                <a>Ayarlar</a>
+              </li>
+              <li>
+                <a>Çıkış Yap</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
