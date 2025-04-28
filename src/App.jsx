5f9522da92ef20ba0eb60325ca882001b59e0eda
ConsolAktif <sigortataciri@gmail.com>
2025-04-28 11:10:02 +0300

    New route components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import "./App.css";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import NotFound from "./pages/NotFound/NotFound";
 import Login from "./pages/Login/Login";
+import useGetUser from "./hooks/useGetUser";
+import Profile from "./pages/Profile/Profile";
+import Apps from "./pages/Apps/Apps";
 
 function App() {
+  const { user, loading } = useGetUser();
+
+  console.log(user);
+
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+        {!user && <Route path="/login" element={<Login />} />}
+        {user && <Route path="/profile" element={<Profile />} />}
+        <Route path="/apps" element={<Apps />} />
+      </Routes>
     </div>
   );
 }
